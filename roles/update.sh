a0e7dceb5ce76ede9b0146c20ea45e68ce06bb32
adz <ahorsewood@googlemail.com>
2019-08-21 11:20:52 +0100

    release

--- roles/update.sh
+++ roles/update.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 cd LaZagne/files
+rm Linux-32bits.zip
+rm Linux-64bits.zip
 wget https://github.com/AlessandroZ/LaZagne/releases/download/1.0/Linux-32bits.zip
 wget https://github.com/AlessandroZ/LaZagne/releases/download/1.0/Linux-64bits.zip
 unzip Linux-32bits.zip
 unzip Linux-64bits.zip
+rm Linux-32bits.zip Linux-64bits.zip
 pwd
 cd ../../LinEnum/files/LinEnum
 git submodule update --init
@@ -13,4 +16,5 @@ git submodule update --init
 cd ../../upc/files/
 git submodule update --init --recursive
 cd ../../linuxprivchecker/files/
+rm linuxprivchecker.py
 wget https://www.securitysift.com/download/linuxprivchecker.py
